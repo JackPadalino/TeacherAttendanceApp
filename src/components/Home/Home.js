584957afeaec74b8fe18a2eb6f331aa5661dedcc
JackPadalino <pada0867@gmail.com>
2023-02-24 15:17:22 -0500

    updated home page greeting

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <>
-          <h1>This is the home page.</h1>  
+          <h1>AMS Staff Attendance</h1>  
         </>
     );
 };
