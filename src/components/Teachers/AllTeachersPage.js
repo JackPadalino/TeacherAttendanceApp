98e166bc19f7ebbb06ee9bff525d0cd11d1d1583
JackPadalino <pada0867@gmail.com>
2023-02-24 20:25:54 -0500

    updated component to use MUI modal

--- src/components/Teachers/AllTeachersPage.js
+++ src/components/Teachers/AllTeachersPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { NotFoundPage } from "..";
 import CreateTeacherForm from './CreateTeacherForm';
+import { Box,Modal,Button,Typography} from '@mui/material';
 
 const pageStyle = {
     display:'flex',
@@ -21,24 +22,53 @@ const buttonStyle={
     color:'blue'
 };
 
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+  };
+
 const AllTeachersPage = () => {
     const { allUsers } = useSelector((state) => state.user);
     const [token, setToken] = useState(window.localStorage.getItem("token"));
-    const [showForm,setShowForm] = useState(false);
-    const [showButton,setShowButton] = useState(true);
+    // variables for MUI components
+    const [parentModalOpen,setParentModalOpen] = useState(false);
 
-    const showHideButton = () =>{
-        setShowForm(true);
-        setShowButton(false);
+    // functions for handling modal
+    const handleParentModal = () => {
+        if(parentModalOpen){
+            setParentModalOpen(false);
+        }else{
+            setParentModalOpen(true);
+        };
     };
     
     if(!token) return <NotFoundPage/>
     return (
         <div style={pageStyle}>
+            <Modal
+                open={parentModalOpen}
+                onClose={handleParentModal}
+                aria-labelledby="parent-modal-title"
+                aria-describedby="parent-modal-description"
+            >
+                <Box sx={{ ...modalStyle, width: 400 }}>
+                    <Typography>Add a teacher</Typography>
+                    <CreateTeacherForm />
+                    {/* <ChildModal /> */}
+                </Box>
+            </Modal>
             <div>
                 <h1 style={headingStyle}>All teachers</h1>
-                {showButton && <button style={buttonStyle} onClick={showHideButton}>Add a teacher</button>}
-                {showForm && <CreateTeacherForm setShowForm={setShowForm} setShowButton={setShowButton}/>}
+                <button style={buttonStyle} onClick={handleParentModal}>Add a teacher</button>
             </div>
             <div>
                 {allUsers.map((user) => {
