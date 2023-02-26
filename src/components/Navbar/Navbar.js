b8aefb03b39d030a94e2f80df73b6950b2c5f49e
JackPadalino <pada0867@gmail.com>
2023-02-25 19:34:34 -0500

    upated styling for navbar using MUI components

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,35 @@ import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link,useNavigate } from "react-router-dom";
 import { resetUser } from "../../store/userSlice";
+import { Box,Typography,TextField,Button} from '@mui/material';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
-const navBarStyle = {
+const navStyle={
   display:'flex',
-  justifyContent:'space-between'
+  justifyContent:"space-between",
+  alignItems:"center"
 };
 
-const navBarLeft = {
+const navCenter={
   display:'flex',
-  gap:'10px'
+  gap:"20px"
+};
+
+const navRight={
+  display:"flex",
+  gap:"10px"
+};
+
+const linkStyle={
+  textDecoration:'none',
+  fontFamily:'Montserrat',
+  color:'black'
+};
+
+const buttonStyle={
+  fontFamily:'Montserrat',
+  color:'black'
 };
 
 const Navbar = () => {
@@ -25,17 +45,22 @@ const Navbar = () => {
   };
 
   return (
-    <div style={navBarStyle}>
-      <div style={navBarLeft}>
-        <Link to="/">Home</Link>
-        {!user.id && <Link to="/login">Login</Link>}
-        {user.id && <Link to="/schedules">Schedules</Link>}
-        {user.id && <Link to="/coverages">Absences/Coverages</Link>}
-        {user.id && <Link to="/teachers">Teachers</Link>}
-        {user.id && <Link to="/classes">Classes</Link>}
+    <div style={navStyle}>
+      <div>
+        <Link style={linkStyle} to="/">AMS Attendance</Link>
       </div>
-      <div className='navBarRight'>
-        {user.id && <button onClick={logout}>Logout</button>}
+      {user.id && <div style={navCenter}>
+        <Link style={linkStyle} to="/schedules">Schedules</Link>
+        <Link style={linkStyle} to="/coverages">Absences/Coverages</Link>
+        <Link style={linkStyle} to="/teachers">Teachers</Link>
+        <Link style={linkStyle} to="/classes">Classes</Link>
+      </div>}
+      <div style={navRight}>
+        {!user.id && <Link style={linkStyle} to="/login">Login</Link>}
+        {user.id && <Fab color="primary" aria-label="add" size="small">
+            <AddIcon />
+        </Fab>}
+        {user.id && <Button onClick={logout} sx={buttonStyle}>Logout</Button>}
       </div>
     </div>
   );
