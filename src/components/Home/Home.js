365116dd256d3ece10c02574c716b17a51a33209
JackPadalino <pada0867@gmail.com>
2023-02-26 09:40:43 -0500

    updated CSS for home page using MUI components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Box,Container,Typography,TextField,Button,Grid,Link,FormControlLabel,Avatar} from '@mui/material';
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const Home = () => {
     return (
-        <>
-          <h1>AMS Staff Attendance</h1>  
-        </>
+      <Container component="main">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap:"10px",
+            placeSelf: "center center",
+            placeItems: "center center",
+            placeContent: "center center",
+            position: "relative",
+            top: "15vh",
+          }}
+        >
+          <Typography 
+            variant="h1"
+            sx={{
+              fontFamily:'Montserrat'
+            }}
+          >
+            Welcome!
+          </Typography>  
+        </Box>
+      </Container>
     );
 };
 
