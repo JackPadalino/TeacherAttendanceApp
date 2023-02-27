7b121b22a4747d0960c8252b89314538d54c5ec6
JackPadalino <pada0867@gmail.com>
2023-02-27 07:08:06 -0500

    added MUI date-picker to DateSelect component - need to fix bugs

--- src/components/Coverages/DateSelect.js
+++ src/components/Coverages/DateSelect.js
@@ -1,17 +1,38 @@
 import axios from 'axios';
-import React from 'react';
+import React,{ useState } from 'react';
 import { useDispatch } from "react-redux";
-import { setCoverageDay,resetCoverageDay,setNewCoverageDate,resetNewCoverageDate,setDateSelected,setAllAbsentUsers,resetAllAbsentUsers } from "../../store/coverageSlice";
+import { 
+    setCoverageDay,
+    resetCoverageDay,
+    setNewCoverageDate,
+    resetNewCoverageDate,
+    setDateSelected,
+    setAllAbsentUsers,
+    resetAllAbsentUsers 
+} from "../../store/coverageSlice";
+import { TextField } from '@mui/material';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
 const DateSelect = () => {
     const dispatch = useDispatch();
 
-    const handleDateChange = async(event) => {
-        event.preventDefault();
+    const [startDate, setStartDate] = useState(dayjs);
+
+    const handleChange = (newValue) => {
+        setValue(newValue);
+        console.log(typeof newValue['$d'].toISOString())
+        console.log(typeof new Date(newValue['$d']));
+    };
+
+    const handleDateChange = async(selectedDate) => {
+        setStartDate(selectedDate)
         dispatch(setDateSelected(true));
 
-        const selectedDate = new Date(event.target.value).toISOString();
-        const foundDay = await axios.get(`/api/day/${selectedDate}`);
+        const dateStr = selectedDate.toISOString();
+        const foundDay = await axios.get(`/api/day/${dateStr}`);
         if(foundDay.data.id){
             dispatch(setCoverageDay(foundDay.data));
             dispatch(resetNewCoverageDate());
@@ -34,6 +55,15 @@ const DateSelect = () => {
                 <label htmlFor="date">Date</label>
                 <input type="date" id="date" onChange={handleDateChange}></input>
             </form>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DesktopDatePicker
+                    label="Date desktop"
+                    inputFormat="MM/DD/YYYY"
+                    value={startDate}
+                    onChange={handleDateChange}
+                    renderInput={(params) => <TextField {...params} />}
+                    />
+            </LocalizationProvider>
         </>
     );
 };
