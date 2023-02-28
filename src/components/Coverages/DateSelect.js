cfae2ac7a6fd3e3d68e4cca8a3dfae3e47e0155f
JackPadalino <pada0867@gmail.com>
2023-02-28 11:02:32 -0500

    have a "band-aid" solution working for date picker - need to figure out time zones

--- src/components/Coverages/DateSelect.js
+++ src/components/Coverages/DateSelect.js
@@ -18,60 +18,58 @@ import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
 const DateSelect = () => {
     const dispatch = useDispatch();
-
     const [startDate, setStartDate] = useState(dayjs);
 
-    const handlePicker1Change = async(event) => {
-        event.preventDefault();
-        dispatch(setDateSelected(true));
-        const selectedDate = new Date(event.target.value).toISOString();
-        console.log(selectedDate);
-        // const foundDay = await axios.get(`/api/day/${selectedDate}`);
-        // console.log(foundDay.data);
-        // if(foundDay.data.id){
-        //     dispatch(setCoverageDay(foundDay.data));
-        //     dispatch(resetNewCoverageDate());
-        //     const absences = await axios.get(`/api/attendance/absences/${selectedDate}`);
-        //     const userPromises = absences.data.map(async (absence) => await axios.get(`/api/users/${absence.user.id}`));
-        //     const userResponses = await Promise.all(userPromises);
-        //     const userAbsences = userResponses.map(response => response.data);
-        //     dispatch(setAllAbsentUsers(userAbsences));
-        // }else{
-        //     dispatch(setNewCoverageDate(selectedDate));
-        //     dispatch(resetCoverageDay());
-        //     dispatch(resetAllAbsentUsers());
+    // const handlePicker1Change = async(event) => {
+    //     event.preventDefault();
+    //     dispatch(setDateSelected(true));
+    //     const selectedDate = new Date(event.target.value).toISOString();
+    //     const foundDay = await axios.get(`/api/day/${selectedDate}`);
+    //     console.log(foundDay.data);
+    //     if(foundDay.data.id){
+    //         dispatch(setCoverageDay(foundDay.data));
+    //         dispatch(resetNewCoverageDate());
+    //         const absences = await axios.get(`/api/attendance/absences/${selectedDate}`);
+    //         const userPromises = absences.data.map(async (absence) => await axios.get(`/api/users/${absence.user.id}`));
+    //         const userResponses = await Promise.all(userPromises);
+    //         const userAbsences = userResponses.map(response => response.data);
+    //         dispatch(setAllAbsentUsers(userAbsences));
+    //     }else{
+    //         dispatch(setNewCoverageDate(selectedDate));
+    //         dispatch(resetCoverageDay());
+    //         dispatch(resetAllAbsentUsers());
 
-        // };
-    };
+    //     };
+    // };
 
     const handleDateChange = async(newValue) => {
         setStartDate(newValue);
-        console.log(newValue.toDate().setHours(0,0,0,0));
-        //console.log(newValue['$d'].setHours(0,0,0,0).toISOString());
-        //const selectedDate = newValue.setHours(0,0,0,0).toISOString();
-        //console.log(selectedDate);
-        // const foundDay = await axios.get(`/api/day/${selectedDate}`);
-        // if(foundDay.data.id){
-        //     dispatch(setCoverageDay(foundDay.data));
-        //     dispatch(resetNewCoverageDate());
-        //     const absences = await axios.get(`/api/attendance/absences/${selectedDate}`);
-        //     const userPromises = absences.data.map(async (absence) => await axios.get(`/api/users/${absence.user.id}`));
-        //     const userResponses = await Promise.all(userPromises);
-        //     const userAbsences = userResponses.map(response => response.data);
-        //     dispatch(setAllAbsentUsers(userAbsences));
-        // }else{
-        //     dispatch(setNewCoverageDate(selectedDate));
-        //     dispatch(resetCoverageDay());
-        //     dispatch(resetAllAbsentUsers());
-        // };
+        console.log();
+        const date1 = newValue.toDate().toISOString().slice(0,10);
+        const date2 = "T00:00:00.000Z";
+        const dateStr=date1+date2;
+        const foundDay = await axios.get(`/api/day/${dateStr}`);
+        if(foundDay.data.id){
+            dispatch(setCoverageDay(foundDay.data));
+            dispatch(resetNewCoverageDate());
+            const absences = await axios.get(`/api/attendance/absences/${dateStr}`);
+            const userPromises = absences.data.map(async (absence) => await axios.get(`/api/users/${absence.user.id}`));
+            const userResponses = await Promise.all(userPromises);
+            const userAbsences = userResponses.map(response => response.data);
+            dispatch(setAllAbsentUsers(userAbsences));
+        }else{
+            dispatch(setNewCoverageDate(dateStr));
+            dispatch(resetCoverageDay());
+            dispatch(resetAllAbsentUsers());
+        };
     };
 
     return (
         <>
-            <form>
+            {/* <form>
                 <label htmlFor="date">Date</label>
                 <input type="date" id="date" onChange={handlePicker1Change}></input>
-            </form>
+            </form> */}
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                     <DesktopDatePicker
                     label="Date desktop"
