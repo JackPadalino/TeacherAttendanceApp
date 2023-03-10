c6ff5f99bb9f2b0ff9b56702be6c3061e050e128
JackPadalino <pada0867@gmail.com>
2023-03-09 22:58:25 -0500

    updated styling of teachers page

---
+++ src/components/Teachers/style.js
@@ -0,0 +1,64 @@
+export const mainContainer = {
+    display: "flex",
+    flexDirection: "column",
+    gap:"50px",
+    //placeSelf: "center",
+    //placeItems: "center",
+    //placeContent: "center center",
+    position: "relative",
+    top: "5vh",
+};
+
+export const headingStyle={
+    fontFamily:"Montserrat",
+    marginBottom:'0px'
+};
+
+export const teacherName = {
+    fontFamily:'Montserrat',
+    fontSize:"22px"
+};
+
+export const buttonStyle={
+    //all:'unset',
+    cursor:'pointer',
+    //textDecoration:'underline',
+    fontFamily:"Montserrat",
+    borderRadius:3,
+    marginBottom:"10px"
+    //color:'blue'
+};
+
+export const parentModalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 200,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 5,
+    px: 2,
+    pb: 5,
+    display:'flex',
+    justifyContent:'center',
+    alignItems:"center",
+  };
+
+export const childModalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 300,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 5,
+    px: 2,
+    pb: 5,
+    display:'flex',
+    justifyContent:'center',
+    alignItems:"center",
+  };
