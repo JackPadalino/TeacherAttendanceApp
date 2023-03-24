740e224145bdb19474a7e66c0383d045cb6ef5e6
JackPadalino <pada0867@gmail.com>
2023-03-23 21:41:47 -0400

    removed files

--- src/components/Teachers/index.js
+++ src/components/Teachers/index.js
@@ -1,5 +1,4 @@
 export { default as SingleTeacherPage } from './SingleTeacherPage';
-export { default as UpdateTeacherForm } from './UpdateTeacherForm';
 export { default as CreateTeacherForm } from './CreateTeacherForm';
 export { default as AllTeachersPage } from './AllTeachersPage';
 export { default as ClassSelect } from './ClassSelect';
